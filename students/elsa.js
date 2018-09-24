22d43f799e19e817be12e508425646b8d0e2f165
Elsa Martinez <elsa.martinez.faucard@gmail.com>
2018-09-24 12:52:25 +0200

    Button added to the page

--- students/elsa.js
+++ students/elsa.js
@@ -1,6 +1,9 @@
-var section = document.querySelector("section.experiments");
-section.classList.add("hidden");
-var button = document.querySelector("button");
-button.addEventListener("click",function(){
-  section.classList.toggle("hidden");
-})
+function main(){
+  var section = document.querySelector("section.experiments");
+  section.classList.add("hidden");
+  var button = document.querySelector("button");
+  button.addEventListener("click",function(){
+    section.classList.toggle("hidden");
+  })
+}
+window.addEventListener('load',main);
